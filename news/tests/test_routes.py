45fda9e4b7a67e5ee7a1eec60d80b1e5d1a41bb3
Serykh Kirill <s.kirill123@mail.ru>
2024-12-06 16:52:41 +0700

    Первая заливка репозитория

---
+++ news/tests/test_routes.py
@@ -0,0 +1,103 @@
+from django.conf import settings
+from django.test import TestCase
+from django.urls import reverse
+from news.models import News, Comment
+from django.contrib.auth import get_user_model
+from http import HTTPStatus
+
+User = get_user_model()
+
+
+class TestHomePage(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        # Создаем 11 новостей, используя bulk_create.
+        News.objects.bulk_create(
+            News(title=f'Новость {index}', text='Просто текст.')
+            for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
+        )
+
+    def test_pages_availability(self):
+        urls = (
+            ('news:home', None),
+            ('news:detail', (1,)),  # Проверяем существующую новость.
+        )
+        for name, args in urls:
+            with self.subTest(name=name):
+                url = reverse(name, args=args)
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_home_page_content(self):
+        url = reverse('news:home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(
+            len(response.context['object_list']),
+            settings.NEWS_COUNT_ON_HOME_PAGE
+        )
+
+    def test_pagination_on_home_page(self):
+        url = reverse('news:home')
+        response = self.client.get(url)
+        self.assertTrue(response.context.get('is_paginated'))
+        response = self.client.get(f"{url}?page=2")
+        self.assertEqual(len(response.context['object_list']), 1)
+
+
+class TestRoutes(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.news = News.objects.create(title='Заголовок', text='Текст')
+        # Создаём двух пользователей с разными именами:
+        cls.author = User.objects.create(username='Лев Толстой')
+        cls.reader = User.objects.create(username='Читатель простой')
+        # От имени одного пользователя создаём комментарий к новости:
+        cls.comment = Comment.objects.create(
+            news=cls.news,
+            author=cls.author,
+            text='Текст комментария'
+        )
+
+    def test_pages_availability(self):
+        urls = (
+            ('news:home', None),
+            ('news:detail', (self.news.id,)),
+            ('users:login', None),
+            ('users:logout', None),
+            ('users:signup', None),
+        )
+        for name, args in urls:
+            with self.subTest(name=name):
+                url = reverse(name, args=args)
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_availability_for_comment_edit_and_delete(self):
+        users_statuses = (
+            (self.author, HTTPStatus.OK),
+            (self.reader, HTTPStatus.NOT_FOUND),
+        )
+        for user, status in users_statuses:
+            self.client.force_login(user)
+            for name in ('news:edit', 'news:delete'):
+                with self.subTest(user=user, name=name):
+                    url = reverse(name, args=(self.comment.id,))
+                    response = self.client.get(url)
+                    self.assertEqual(response.status_code, status)
+
+    def test_redirect_for_anonymous_client(self):
+        login_url = reverse('users:login')
+        for name in ('news:edit', 'news:delete'):
+            with self.subTest(name=name):
+                url = reverse(name, args=(self.comment.id,))
+                redirect_url = f'{login_url}?next={url}'
+                response = self.client.get(url)
+                self.assertRedirects(response, redirect_url)
+
+    def test_home_page(self):
+        url = reverse('news:home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
